2b8f0a029c556f0513daaa3f3a8986ab6f746303
Dmitrii Kirshanov <dmitrii.kirshanov@macys.com>
2023-10-05 17:56:48 +0400

    feat: add button element

---
+++ src/shared/button/ui/button.vue
@@ -0,0 +1,17 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<style scoped>
+button {
+  border-radius: 10em;
+  border: 2px solid var(--theme-color-2);
+  padding: 0.75em 2em;
+  color: var(--theme-color-2);
+  background-color: var(--text-color);
+  font-size: 1rem;
+  cursor: pointer;
+}
+</style>
