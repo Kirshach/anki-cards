ea0d65e14c71bf29290c907ecf7e79881d44f92d
Dmitrii Kirshanov <dmitrii.kirshanov@macys.com>
2023-10-05 17:21:49 +0400

    feat: add Kumbh Sans font

--- src/shared/text-input/ui/text-input.vue
+++ src/shared/text-input/ui/text-input.vue
@@ -5,15 +5,16 @@
 <style scoped>
 input {
   padding: 0.75em 1em;
-  border: 5px solid var(--theme-color-3);
+  border: 5px solid var(--theme-color-2);
   border-radius: 10em;
   font-size: 1rem;
   outline: 3px solid transparent;
   transition: outline 0.2s ease-out;
 }
 
-input:focus-visible {
-  outline: 3px solid var(--theme-color-1);
+input:focus {
+  outline: 3px solid var(--theme-color-accent);
+  outline-offset: 2px;
   transition: outline 0.1s ease-in-out;
 }
 </style>
