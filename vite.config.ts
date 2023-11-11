d0cde2352a8f9417e91cdd0fab3595547f999ffe
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-11 17:00:44 +0400

    update ssl

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: { https: true },
-  plugins: [vue(), mkcert()],
+//  server: { https: true },
+  plugins: [vue()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
