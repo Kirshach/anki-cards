1fa1f33353a5138bee15ca9e3014e67e7496224a
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-11 13:52:04 +0400

    try to fix ssl

--- vite.config.ts
+++ vite.config.ts
@@ -6,8 +6,8 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-//  server: { https: true },
-  plugins: [vue()],
+  server: { https: true },
+  plugins: [vue(), mkcert()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
