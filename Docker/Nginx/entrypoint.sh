6b5ef88bc50b929ba07763e7d902743cd7977e0d
Romashov-Zima Dmitry <romashov.dmitry.py@gmail.com>
2023-11-14 23:09:52 +0400

    update nginx

--- Docker/Nginx/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Подставляем переменные окружения в шаблон конфигурации
-envsubst < /etc/nginx/conf.d/nginx.conf > /etc/nginx/conf.d/default.conf
-chmod +x /etc/nginx/conf.d/default.conf
-
-exec nginx -g 'daemon off;'
